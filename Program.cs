0d99c28271b238966793381adfdc93e17045cc22
Lev Filippov <filippov.l.a.89@gmail.com>
2022-12-20 20:34:43 +0700

    Задача на сравнение 2-х чисел

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Enter the first argument (only whole numbers).");
+long firstArg;
+long secondArg;
+String message = "You entered wrong argument!";
+try{
+    firstArg =  long.Parse(Console.ReadLine() ?? "0");
+} catch (Exception e) {
+    Console.WriteLine();
+    throw e;
+}
+Console.WriteLine(message);
+try{
+    secondArg =  long.Parse(Console.ReadLine() ?? "0");
+} catch (Exception e) {
+    Console.WriteLine(message);
+    throw e;
+}
+
+if(firstArg> secondArg) {
+    Console.WriteLine($"{firstArg} is bigger than {secondArg}");
+    return;
+} 
+Console.WriteLine($"{secondArg} is bigger than {firstArg}");
+
